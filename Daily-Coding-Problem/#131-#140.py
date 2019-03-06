7441d5ea7336c76d1707e2c2a3d6ee72cfefa1fc
minhhoangbui <minhhoang0602kc@gmail.com>
2019-03-06 23:42:54 +0700

    before

---
+++ Daily-Coding-Problem/#131-#140.py
@@ -0,0 +1,19 @@
+'''Solve Daily Coding Problems from #121-#130'''
+from outils import LinkedBinaryTree
+
+def prob_135(tree):
+    '''
+    Given a binary tree, find a minimum path sum from root to a leaf.
+
+    For example, the minimum path in this tree is [10, 5, 1, -1], which has sum 15.
+
+        10
+       /  \
+      5    5
+       \     \
+        2    1
+            /
+          -1
+    '''
+    pass
+    
--- Daily-Coding-Problem/outils.py
+++ Daily-Coding-Problem/outils.py
@@ -1,5 +1,6 @@
 ''' Some common classes which helps us to complete the assignment'''
 from abc import ABCMeta, abstractmethod
+from collections import MutableMapping
 
 class Stack(object):
     def __init__(self):
@@ -466,7 +467,211 @@ class Graph:
         edge = self._Edge(u, v, x)
         self._outgoing[u][v] = edge
         self._incoming[v][u] = edge
+
+class MapBase(MutableMapping):
+    class _Item:
+        def __init__(self, key, value):
+            self._key = key
+            self._value = value
+        
+        def __eq__(self, other):
+            return self._key == other._key
+        
+        def __ne__(self, other):
+            return not (self == other)
+        
+        def __lt__(self, other):
+            return self._key < other._key
+
+class TreeMap(LinkedBinaryTree, MapBase):
+
+    class Position(LinkedBinaryTree.Position):
+        def key(self):
+            return self.element()._key
+
+        def value(self):
+            return self.element()._value
     
+    def _subtree_search(self, p, k):
+        if k == p.key():
+            return p
+        elif k < p.key():
+            if self.left(p) is not None:
+                self._subtree_search(self.left(p), k)
+        else:
+            if self.right(p) is not None:
+                self._subtree_search(self.right(p), k)
+        return p
+
+    def _subtree_first_position(self, p):
+        walk = p
+        while self.left(p) is not None:
+            walk = self.left(p)
+        return walk
+
+    def _subtree_last_position(self, p):
+        walk = p
+        while self.right(p) is not None:
+            walk = self.right(p)
+        return walk
+    
+    def _rebalance_access(self, p):
+        raise NotImplementedError
+    
+    def _rebalance_insert(self, p):
+        raise NotImplementedError
+
+    def _rebalance_delete(self, p):
+        raise NotImplementedError
+
+    def first(self):
+        return self._subtree_first_position(self.root())    
+    
+    def last(self):
+        return self._subtree_last_position(self.root())
+
+    def before(self, p):
+        self._validate(p)
+        if self.left(p) is not None:
+            return self._subtree_last_position(self.left(p))
+        else:
+            walk = p
+            above = self.parent(walk)
+            while above is not None and walk == self.left(above):
+                walk = above
+                above = self.parent(walk)
+            return above
+    
+    def after(self, p):
+        self._validate(p)
+        if self.right(p) is not None:
+            return self._subtree_first_position(self.right(p))
+        else:
+            walk = p
+            above = self.parent(walk)
+            while above is not None and walk == self.right(above):
+                walk = above
+                above = self.right(walk)
+            return above
+    
+    def find_position(self, k):
+        if self.is_empty():
+            return None
+        else:
+            p = self._subtree_search(self.root(), k)
+            self._rebalance_access(p)
+            return p
+    
+    def find_min(self):
+        if self.is_empty():
+            return None
+        else:
+            p = self.first()
+            return (p.key(), p.value())
+    
+    def find_range(self, start, stop):
+        if not self.is_empty():
+            if start is None:
+                p = self.first()
+            else:
+                p = self.find_position(start)
+                if p.key() < start:
+                    p = self.after(p)
+            while p is not None and (stop is None or p.key() < stop):
+                yield (p.key(), p.value())
+                p = self.after(p)
+    
+    def __getitem__(self, k):
+        if self.is_empty():
+            raise KeyError('Key Error:' + str(k))
+        else:
+            p = self._subtree_search(self.root(), k)
+            self._rebalance_access(p)
+            if k != p.key():
+                raise KeyError('Key Error' + str(k))
+            return p.value()
+    
+    def __setitem__(self, k, v):
+        if self.is_empty():
+            leaf = self.add_root(self._Item(k, v))
+        else:
+            p = self._subtree_search(self.root(), k)
+            if k == p.key():
+                p.element()._value = v
+                self._rebalance_access(p)
+                return
+            else:
+                item = self._Item(k, v)
+                if p.key() < k:
+                    leaf = self.add_right(p, item)
+                else:
+                    leaf = self.add_left(p, item)
+        self._rebalance_insert(leaf)
+    
+    def __iter__(self):
+        p = self.first()
+        while p is not None:
+            yield p.key()
+            p = self.after(p)
+    
+    def _delete(self, p):
+        self._validate(p)
+        if self.left(p) and self.right(p):
+            replacement = self._subtree_last_position(self.left(p))
+            self.replace(p, replacement.element())
+            p = replacement
+        parent = self.parent(p)
+        self.delete(p)
+        self._rebalance_delete(parent)
+    
+    def __delitem__(self, k):
+        if not self.is_empty():
+            p = self._subtree_search(self.root(), k)
+            if k == p.key():
+                self._delete(p)
+                return 
+            self._rebalance_access(p)
+        raise KeyError
+    
+    def _relink(self, parent, child, make_left_child):
+        if make_left_child:
+            parent._left = child
+        else:
+            parent._right = child
+        child._parent = parent
+    
+    def _rotate(self, p):
+        self._validate(p)
+        x = p._node
+        y = x._parent
+        z = y._parent
+
+        if z is None:
+            self._root = x
+            x._parent = None
+        else:
+            self._relink(z, x, y == z._left)
+        
+        if x == y._left:
+            self._relink(y, x._right, True)
+            self._relink(x, y, False)
+        else:
+            self._relink(y, x_left, False)
+            self._relink(x, y, True)
+
+    def _restructure(self, x):
+        y = self.parent(x)
+        z = self.parent(y)
+        if (x == self.left(y)) == (y == self.left(z)):
+            self._rotate(y)
+            return y
+        else:
+            self._rotate(x)
+            self._rotate(x)
+            return x
+        
+
+        
 
     
 if __name__ == "__main__":
